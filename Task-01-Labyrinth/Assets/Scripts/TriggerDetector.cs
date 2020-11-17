56a509709634421c0536ba82536ea25106b62878
helenwub <helina123konjo@gmail.com>
2020-11-17 11:52:06 -0600

    all

--- Task-01-Labyrinth/Assets/Scripts/TriggerDetector.cs
+++ Task-01-Labyrinth/Assets/Scripts/TriggerDetector.cs
@@ -20,21 +20,5 @@ public class TriggerDetector : MonoBehaviour
 
         if (gameObject.name == "End" && collider.gameObject.name.Contains("Player-"))
             labyrinthCompleteScript.Completed();
-        else if(gameObject.name.Contains("Move-") && collider.gameObject.name.Contains("Player-"))
-        {
-            //add new Special Move to Player's bank of available Special Moves
-            //TODO: Find another way to cleanly handle a variety of moves
-            if (gameObject.name == "Move-Jump")
-                playerController.AddSpecialMove(new SMJump(collider.transform));
-            else if (gameObject.name == "Move-Grow")
-                playerController.AddSpecialMove(new SMGrow(collider.transform));
-            else if (gameObject.name == "Move-Shrink")
-                playerController.AddSpecialMove(new SMShrink(collider.transform));
-            else if (gameObject.name == "Move-Thomas")
-                playerController.AddSpecialMove(new SMThomas(collider.transform));
-
-            //de-activate Special Move game object
-            Destroy(gameObject);
-        }
     }
 }
